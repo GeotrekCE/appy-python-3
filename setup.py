604db0552f5607ac6167ff8875d2aed5e91b5ab4
Gaël UTARD <gael.utard@makina-corpus.com>
2019-11-19 10:31:48 +0100

    Adapt to release on pypi

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ def find_packages():
         r.append(package_name)
     return r
 
-setup(name="appy",
-      version="1.99.dev0",
-      description="The Appy framework",
-      long_description="Appy builds simple but complex web Python apps.",
+setup(name="appy-python-3",
+      version="2019.11.19",
+      description="The Appy framework (Python 3)",
+      long_description="Unofficial pypi release of offical code from https://forge.pallavi.be/projects/appy-python-3.",
       author="Gaetan Delannay",
-      author_email="gaetan.delannay AT geezteem.com",
+      author_email="gaetan.delannay@geezteem.com",
       license="GPL",
       platforms="all",
       url='http://appyframework.org',
